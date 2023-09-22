50f6c22c88e019bb1160ef6b806559aac9e5a840
Gabriel Bealer <bealeg13@gmail.com>
2023-09-22 08:25:12 -0700

    fixed error

--- SQLJoinsExercise.sql
+++ SQLJoinsExercise.sql
@@ -1,10 +1,10 @@
 /* joins: select all the computers from the products table:
 using the products table and the categories table, return the product name and the category name */
-SELECT products.Name, categories.Name
-FROM products 
-INNER JOIN categories 
-ON products.CategoryID = categories.CategoryID 
-WHERE categories.Name = 'computers';
+ SELECT products.Name, categories.Name
+ FROM products AS products
+ INNER JOIN categories AS categories
+ ON products.CategoryID = categories.CategoryID 
+ WHERE categories.Name = 'computers';
 
 
 /* joins: find all product names, product prices, and products ratings that have a rating of 5 */
@@ -21,10 +21,12 @@ FROM employees
 INNER JOIN sales
 ON employees.EmployeeID = sales.EmployeeID 
 GROUP BY employees.EmployeeID
-ORDER BY Quantity DESC;
+ORDER BY Quantity DESC
+LIMIT 2;
+
 
 /* joins: find the name of the department, and the name of the category for Appliances and Games */
-SELECT departments.Name, categories.Name
+SELECT departments.Name as "Department Name", categories.Name as "Category Name"
 FROM departments
 INNER JOIN categories
 ON departments.DepartmentID = categories.DepartmentID
@@ -33,12 +35,13 @@ WHERE categories.Name = "Appliances" OR categories.Name = "Games";
 
 /* joins: find the product name, total # sold, and total price sold,
  for Eagles: Hotel California --You may need to use SUM() */
-SELECT products.Name, SUM(sales.Quantity) AS SUMSALES, SUM(sales.Quantity * products.Price) AS SUMPRICE
-FROM sales
-INNER JOIN products
+SELECT products.Name, SUM(sales.Quantity) as "Total # sold", 
+SUM(sales.Quantity * sales.PricePerUnit) as "Total price sold"
+FROM products
+INNER JOIN sales
 ON products.ProductID = sales.ProductID
-WHERE products.Name = "Hotel California"
-GROUP BY products.Name;
+WHERE products.Name LIKE "Eagles: Hotel California";
+
  
 
 /* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
@@ -51,3 +54,20 @@ SELECT MIN(Rating)
 FROM reviews
 WHERE products.Name = "Visio TV"
 );
+
+
+
+/* Your goal is to write a query that serves as an employee sales report.
+This query should return:
+-  the employeeID
+-  the employee's first and last name
+-  the name of each product
+-  and how many of that product they sold */
+
+
+SELECT employees.EmployeeID, employees.FirstName, employees.LastName, products.Name, Sum(sales.Quantity)
+FROM employees
+INNER JOIN sales on employees.EmployeeID = sales.EmployeeID
+INNER JOIN products on sales.ProductID = products.ProductID
+GROUP BY employees.EmployeeID, products.ProductID
+ORDER BY employees.FirstName;
